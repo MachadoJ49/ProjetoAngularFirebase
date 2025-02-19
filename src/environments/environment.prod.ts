ff4c1f4927eea5e2b9828e748b75577766802e80
MachadoJ49 <machado.JuliaAntunes49@gmail.com>
2025-02-19 17:53:14 -0300

    Att

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
-    authDomain: "aula-3e82a.firebaseapp.com",
-    projectId: "aula-3e82a",
-    storageBucket: "aula-3e82a.firebasestorage.app",
-    messagingSenderId: "258284807710",
-    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
-    measurementId: "G-X8L8GS7897"
+    apiKey: "AIzaSyChh2onBKGJwDqa921hMEcS8bK8-yUJ3W4",
+    authDomain: "aula1-fe047.firebaseapp.com",
+    projectId: "aula1-fe047",
+    storageBucket: "aula1-fe047.firebasestorage.app",
+    messagingSenderId: "356995991740",
+    appId: "1:356995991740:web:cd205ad6ce54b4e79e68b9",
+    measurementId: "G-TTTFQEJP8N"
   },
   production: true
 };
